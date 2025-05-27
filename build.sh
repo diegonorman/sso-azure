82199cf00a415259a120e773ae767b84660d2670
diegonorman <diegonorman5@gmail.com>
2025-05-27 14:41:23 -0300

    add build.sh for Render deploy

--- build.sh
+++ build.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
+# Instala dependências de sistema necessárias para o SAML
 apt-get update && apt-get install -y libxml2-dev libxmlsec1-dev pkg-config xmlsec1
+# Instala dependências Python
+pip install --upgrade pip
 pip install -r requirements.txt
+# Aplica as migrações do Django
 python manage.py migrate
