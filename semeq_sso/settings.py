b1711484b61fad259ac116b1ac9ac9557d5e8d31
diegonorman <diegonorman5@gmail.com>
2025-05-27 16:55:49 -0300

    Ajuste SAML_CONFIG: want_assertions_signed=False, want_response_signed=True para compatibilidade Azure AD

--- semeq_sso/settings.py
+++ semeq_sso/settings.py
@@ -169,8 +169,8 @@ SAML_CONFIG = {
             'allow_unsolicited': True,
             'authn_requests_signed': False,
             'logout_requests_signed': True,
-            'want_assertions_signed': True,
-            'want_response_signed': False,
+            'want_assertions_signed': False,  # Azure AD não assina Assertion
+            'want_response_signed': True,    # Azure AD assina o Response
         },
     },
     'metadata': {
